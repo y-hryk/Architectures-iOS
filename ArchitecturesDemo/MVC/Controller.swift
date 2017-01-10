289ed11181e7c73075b75b47a566bd5be494706c
y-hryk <dev.hy630823@gmail.com>
2017-01-10 19:06:16 +0900

    ファイル追加

---
+++ ArchitecturesDemo/MVC/Controller.swift
@@ -0,0 +1,35 @@
+//
+//  Controller.swift
+//  ArchitecturesDemo
+//
+//  Created by h.yamaguchi on 2017/01/10.
+//  Copyright © 2017年 h.yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ArchitecturesDemo/MVC/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  ArchitecturesDemo
+//
+//  Created by h.yamaguchi on 2017/01/10.
+//  Copyright © 2017年 h.yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class Movie: NSObject {
+
+}
